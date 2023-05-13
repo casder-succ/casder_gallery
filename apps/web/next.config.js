b8a74500b610594ea925ad1330e8c7bb07a33953
Vadim Samal <casderiopus@gmail.com>
2023-05-13 15:58:25 +0300

    initial commit

---
+++ apps/web/next.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  output: 'standalone',
+  pageExtensions: ['page.tsx', 'api.ts'],
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
